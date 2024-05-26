87b768836fb46bf570affd482f70e4dee66745be
IRTD <reisbaellchen17@gmail.com>
2024-05-26 09:18:14 +0200

    Better install file

--- install/install.sh
+++ install/install.sh
@@ -1,3 +1,11 @@
+clear
+echo "Installing Rust..."
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+. "$HOME/.cargo/env"
+rustup default stable
+
+clear
 echo "Installing paru..."
 
 sudo pacman -S git base-devel --needed
@@ -7,31 +15,34 @@ makepkg -si
 cd ..
 rm -rf paru
 
+clear
 echo "Paru installed, updating Mirrors and System..."
 paru -Syu
 
+clear
 echo "Installing Packages..."
 for pkg in $(cat pkgs)
 do
 	paru -S $pkg --needed
 done
 
-echo "Installing Rust..."
-curl --proto '=hhtps' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
-. "$HOME/.cargo/env"
-
+clear
 echo "Installing Cargo binaries..."
 for cbin in $(cat cargo-bins)
 do
 	cargo install --locked $cbin
 done
 
+clear
 echo "Fetching dotfiles..."
 git clone https://github.com/IRTD/dotfiles ~/dotfiles
 cd ~/dotfiles 
+git switch origin hypr-new
+git pull origin hypr-new
 stow . 
- 
+
+clear
 echo "Installing EWW bar..." 
 git clone https://github.com/elkowar/eww
 cd eww
@@ -40,10 +51,21 @@ cp target/release/eww ~/.cargo/bin
 cd ..
 rm -rf eww
 
+clear
 echo "Installing flatpak packages..."
 for flatpkg in $(cat flatpak-apps)
 do
     flatpak install $flatpkg
-end
+done
 
-echo "Everything done, enjoy!"
+clear
+echo "Setting zsh as default shell"
+chsh -s /bin/zsh
+
+clear
+echo "Fetching nvim config..."
+git clone https://github.com/IRTD/nvim ~/.config/nvim
+
+clear
+echo "Everything done, rebooting..."
+reboot
